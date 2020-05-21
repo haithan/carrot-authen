9509e3885745d2afe9be418f12aeff5e196e32e2
jaherulalom <jaherul@live.co.uk>
2020-05-21 16:27:25 +0100

    setup password reset

--- api/handlers/auth/login.js
+++ api/handlers/auth/login.js
@@ -1,31 +1,44 @@
-const passport = require('passport');
-const passportLocal = require('passport-local');
-const User = require('../../database/User');
-const { createToken } = require('../../utils/jwt-token');
+const passport = require("passport");
+const passportLocal = require("passport-local");
+const User = require("../../database/models/User");
+const { createToken } = require("../../utils/jwt-token");
 
-passport.use('login', new passportLocal.Strategy({
-  usernameField: 'email',
-  passwordField: 'password',
-  session: false,
-}, (email, password, done) => {
-  try {
-    User.findOne({ where: { email } }).then((user) => {
-      if (user === null) {
-        return done(null, false, { auth: false, message: 'invalid credentials' });
-      } else {
-        user.validatePassword(password).then((validated) => {
-          if (!validated) return done(null, false, { auth: false, message: 'invalid credentials' });
-          return done(null, user); 
+passport.use(
+  "login",
+  new passportLocal.Strategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+      session: false,
+    },
+    (email, password, done) => {
+      try {
+        User.findOne({ where: { email } }).then((user) => {
+          if (user === null) {
+            return done(null, false, {
+              auth: false,
+              message: "invalid credentials",
+            });
+          } else {
+            user.validatePassword(password).then((validated) => {
+              if (!validated)
+                return done(null, false, {
+                  auth: false,
+                  message: "invalid credentials",
+                });
+              return done(null, user);
+            });
+          }
         });
+      } catch (err) {
+        return done(err);
       }
-    });
-  } catch (err) {
-    return done(err);
-  }
-}));
+    }
+  )
+);
 
 module.exports = async (req, res, next) => {
-  passport.authenticate('login', (err, user, info) => {
+  passport.authenticate("login", (err, user, info) => {
     if (err) return next(err);
     if (info) res.json(info);
     req.logIn(user, (err) => {
--- config/default.js
+++ config/default.js
@@ -1,10 +1,14 @@
-const fs = require('fs');
+const fs = require("fs");
 module.exports = {
   jwt: {
-    privateKey: fs.readFileSync('./config/certs/private.key', 'utf8'),
-    publicKey: fs.readFileSync('./config/certs/public.key', 'utf8'),
+    privateKey: fs.readFileSync("./config/certs/private.key", "utf8"),
+    publicKey: fs.readFileSync("./config/certs/public.key", "utf8"),
   },
   db: {
-    uri: 'mariadb://admin:1qaz2wsx@carrott-node.cmoyikxbbpvu.eu-west-2.rds.amazonaws.com/carrott',
+    uri:
+      "mariadb://admin:1qaz2wsx@carrott-node.cmoyikxbbpvu.eu-west-2.rds.amazonaws.com/carrott",
+  },
+  mailer: {
+    password: "18260432Slk!",
   },
 };
