62c6fb3c8e7a922557aca0801e73e4bd48d5128d
Thuan <thuan.nguyen@solid.com>
2021-01-27 17:53:41 +0700

    update password validator

--- api/handlers/passwordChange/validate.js
+++ api/handlers/passwordChange/validate.js
@@ -4,7 +4,7 @@ module.exports = [
   check("current_password", "Current password cannot be empty").notEmpty(),
   check("new_password", "New password cannot be empty").notEmpty(),
   check("new_password", "Invalid new password").matches(
-    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    /^(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
     "i"
   ),
 ];
